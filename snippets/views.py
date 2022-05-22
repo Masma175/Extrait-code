d79c045de8f135a94404fbe354d63fdfa45e63b5
Masma175 <jephtemasma8@gmail.com>
2022-05-22 20:34:53 +0200

    implementation de la Vue detail avec l'update et la modification de l'extrait

--- snippets/views.py
+++ snippets/views.py
@@ -15,10 +15,31 @@ def snippet_list(request):
         return JsonResponse(serializer.data, safe=False)
     
     elif request.method == "POST":
-        data = JSONParser().parser(request)
+        data = JSONParser().parse(request)
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """ Affichage , modification et suppression d'un extrait de code """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == "PUT":
+        data = JSONParser().parser(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    elif request.method == "DELETE":
+        snippet.delete()
+        return HttpResponse(status=204)
